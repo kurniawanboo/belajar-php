a2ca2491def2d724f73ae2ce6be67864ea479cc2
Kurniawan Bonenehu <booawan@gmail.com>
2022-10-28 19:52:10 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+echo "Hello World!<br>";
+
+$nama = "Kurniawan Bonenehu";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya $umur tahun.<br>";
+
+$namaAdik = "Tarmisi";
+$umurKakak = 28 ;
+
+echo "Nama Kakak saya <strong>$namaKakak</strong>, Kakak saya berusia $umurKakak tahun.<br>";
+
+
+$selisihUmur = $umur - $umurKakak;
+
+echo "selisih usia saya dan Kakak saya adalah $selisihUmur tahun";
+
+?>
